213994ba05dca21355b53787691c214b92512337
Omer1015 <omerelbashir15@gmail.com>
2024-11-14 22:49:29 +0400

    Added an Image through Express on localhost

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
-var express = require("express"); // Requires the Express module
-var http = require('http');
-// Calls the express function to start a new Express application
-var app = express();
-app.use(function(request, response) { // middleware
- console.log("In comes a request to: " + request.url);
- response.end("Hello, world!");
+const express = require("express"); // Requires the Express module
+const path = require("path");
+const fs = require("fs");
+
+const app = express(); // Calls the express function to start a new Express application
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log("Request URL:", req.url);
+  console.log("Request Date:", new Date());
+  next();
 });
-http.createServer(app).listen(3000); // start the server
+
+// Middleware to serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Direct file-serving middleware for debugging
+app.use((req, res, next) => {
+  const filePath = path.join(__dirname, "public", req.url);
+  console.log("Attempting to serve file from:", filePath);
+  
+  fs.stat(filePath, (err, fileInfo) => {
+    if (err) {
+      console.log("File not found or error accessing file:", err);
+      next(); // Pass to 404 handler
+      return;
+    }
+
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+      next();
+    }
+  });
+});
+
+// 404 Middleware
+app.use((req, res) => {
+  res.status(404).send("Hello World");
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("App started on port 3000");
+});
