09b4ffb9a15320a5bfdde0148cc3ce7775c54ba4
Omer1015 <omerelbashir15@gmail.com>
2024-11-14 20:42:59 +0400

    added mustache and hello world example

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-// Starts the server on port 8080
-app.listen(8080, () => {
-    console.log("Server is listening on port 8080.");
-});
+var http = require("http"); // Requires the built-in http module
+// Defines a function that’ll handle incoming HTTP requests
+function requestHandler(request, response) {
+console.log("Incoming request to: " + request.url);
+ response.end("Hello, world!");
+}
+// Creates a server that uses your function to handle requests
+var server = http.createServer(requestHandler);
+// Starts the server listening on port 3000
+server.listen(3000);
